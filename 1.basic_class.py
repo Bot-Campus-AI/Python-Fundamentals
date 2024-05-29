cb31c0425382812fd5963fd8e74ede883dc1f9f8
Abdullah Khan <abdullah@khan.com>
2024-05-30 03:59:54 +0530

    Modifying a couple of files

--- 1.basic_class.py
+++ 1.basic_class.py
@@ -1,6 +1,7 @@
 class Dog:
     pass
 
+
 # Creating an object
 my_dog = Dog()
 print(my_dog)
---
+++ 2.constructors.py
@@ -0,0 +1,7 @@
+class Dog:
+    def __init__(self):
+        print("A dog has been created!")
+
+
+# Creating an instance of the Dog class
+my_dog = Dog()
